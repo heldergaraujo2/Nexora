39ed4612f1635e562de3f94916e801dd083bebaf
heldergaraujo2 <heldergaraujo2@gmail.com>
2026-09-11 18:07:46 -0300

    feat: export TOML policy loader

--- src/nexora/governanca/__init__.py
+++ src/nexora/governanca/__init__.py
@@ -1,5 +1,12 @@
 """Governanca e politicas da NEXORA."""
 
 from .policy import DecisaoPolitica, EfeitoPolitica, PolicyEngine, RegraPolitica
+from .policy_loader import carregar_policy_toml
 
-__all__ = ["DecisaoPolitica", "EfeitoPolitica", "PolicyEngine", "RegraPolitica"]
+__all__ = [
+    "DecisaoPolitica",
+    "EfeitoPolitica",
+    "PolicyEngine",
+    "RegraPolitica",
+    "carregar_policy_toml",
+]
