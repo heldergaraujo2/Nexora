0662dd140dcffaaf370aa6febb4ab74f8161f0d4
heldergaraujo2 <heldergaraujo2@gmail.com>
2026-09-11 17:57:06 -0300

    feat: export policy engine

---
+++ src/nexora/governanca/__init__.py
@@ -0,0 +1,5 @@
+"""Governanca e politicas da NEXORA."""
+
+from .policy import DecisaoPolitica, EfeitoPolitica, PolicyEngine, RegraPolitica
+
+__all__ = ["DecisaoPolitica", "EfeitoPolitica", "PolicyEngine", "RegraPolitica"]
