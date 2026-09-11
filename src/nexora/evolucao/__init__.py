d6404410856060eff9f13f6ac711f12dccb57708
heldergaraujo2 <heldergaraujo2@gmail.com>
2026-09-11 16:30:58 -0300

    fix: expose evolution engine package

---
+++ src/nexora/evolucao/__init__.py
@@ -0,0 +1,5 @@
+"""API pública do Evolution Engine da NEXORA."""
+
+from .registro import Aprendizado, RegistroAprendizados, RecomendadorEvolucao
+
+__all__ = ["Aprendizado", "RegistroAprendizados", "RecomendadorEvolucao"]
