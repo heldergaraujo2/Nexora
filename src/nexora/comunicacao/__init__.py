273cfcb921e0e7852ed9d7bc9f9e01e28db50429
heldergaraujo2 <heldergaraujo2@gmail.com>
2026-09-11 16:56:59 -0300

    feat: add agent communication bus package

---
+++ src/nexora/comunicacao/__init__.py
@@ -0,0 +1,5 @@
+"""Communication Bus da NEXORA para comunicação entre agentes."""
+
+from .bus import CommunicationBus, EstadoMensagem, MensagemAgente
+
+__all__ = ["CommunicationBus", "EstadoMensagem", "MensagemAgente"]
